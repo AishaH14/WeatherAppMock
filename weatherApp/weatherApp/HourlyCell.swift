f3af3651d64fe1b0c9cc4b97ba952c65abffffca
AishaH14 <AHudasi@innovationteam.com>
2026-03-09 15:58:13 +0300

    feat: update table view layout and add weather data setup in ViewController

---
+++ weatherApp/weatherApp/HourlyCell.swift
@@ -0,0 +1,15 @@
+//
+//  HourlyCell.swift
+//  weatherApp
+//
+//  Created by Aisha Hudasi on 19/09/1447 AH.
+//
+
+import UIKit
+
+class HourlyCell: UICollectionViewCell {
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var tempLabel: UILabel!
+}
--- weatherApp/weatherApp/ViewController.swift
+++ weatherApp/weatherApp/ViewController.swift
@@ -4,18 +4,45 @@
 //
 //  Created by Aisha Hudasi on 15/09/1447 AH.
 //
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    @IBOutlet weak var topCard: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        topCard.layer.cornerRadius = 24
-        topCard.clipsToBounds = true
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UINib(nibName: "CurrentWeatherCell", bundle: nil),
+                                  forCellReuseIdentifier: "CurrentWeatherCell")
+        tableView.register(UINib(nibName: "HourlyCell", bundle: nil),
+                              forCellReuseIdentifier: "HourlyCell")
     }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return 1
+       }
 
-}
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CurrentWeatherCell", for: indexPath) as! CurrentWeatherCell
+
+            cell.cityLabel.text = "Jeddah"
+            cell.tempLabel.text = "24°"
+            cell.weatherLabel.text = "Sunny"
+            cell.weatherImage.image = UIImage(systemName: "location.fill")
+            cell.weatherImage.tintColor = .white
+
+            return cell
+        }
+        
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 220
+        }
+    }
+    
+
 
