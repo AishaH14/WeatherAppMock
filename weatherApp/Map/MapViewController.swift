10dc79328ac8a66ec4b87b7a94e545a955092c80
AishaH14 <AHudasi@innovationteam.com>
2026-04-16 11:38:26 +0300

    feat: add MapViewController screen and xib for map feature

---
+++ weatherApp/Map/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  weatherApp
+//
+//  Created by Aisha Hudasi on 28/10/1447 AH.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+
+    
+
+}
